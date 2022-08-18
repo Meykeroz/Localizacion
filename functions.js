7fa3bfc32f96cd742925a94ca0275ee8b5d4263d
Manuel Alvarez <44854788+Meykeroz@users.noreply.github.com>
2022-08-17 20:12:28 -0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -10,3 +10,17 @@ function geo(geolocationPosition){
 
     window.open("https://www.google.com/maps/dir/'"+coords.latitude+","+coords.longitude+"'/'32.662785919276786,-115.49542096026951'");
 }
+    let divice = navigator.userAgent;
+
+    if(navigator.userAgent.match(/Android/i) ||
+       navigator.userAgent.match(/webOS/i) ||
+       navigator.userAgent.match(/iPhone/i) || 
+       navigator.userAgent.match(/iPad/i) ||
+       navigator.userAgent.match(/BlackBerry/i) ||
+       navigator.userAgent.match(/iPod/i) ||
+       navigator.userAgent.match(/Windows Phone/i))
+       {
+        console.log("Estas utilizando un dispositivo movil.");
+       }else{
+        console.log("Estas utilizando un dispositivo de escritorio.");
+       }
